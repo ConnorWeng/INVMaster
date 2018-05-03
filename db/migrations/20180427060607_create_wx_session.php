8988011396025e4898df6d7e18591c50d2960171
翁晓俊 <wengxj@sdc.icbc.com.cn>
2018-05-03 22:31:35 +0800

    timestamp字段默认值调整为CURRENT_TIMESTAMP

--- db/migrations/20180427060607_create_wx_session.php
+++ db/migrations/20180427060607_create_wx_session.php
@@ -34,8 +34,8 @@ class CreateWxSession extends AbstractMigration
             ->addColumn('open_id', 'string', array('limit' => 100,'default'=>'','comment'=>'微信用户id'))
             ->addColumn('uuid', 'string', array('limit' => 100,'default'=>'','comment'=>'uuid'))
             ->addColumn('skey', 'string', array('limit' => 100,'default'=>'','comment'=>'会话id'))
-            ->addColumn('create_time', 'timestamp', array('default'=>CURRENT_TIMESTAMP,'comment'=>'创建时间'))
-            ->addColumn('last_visit_time', 'timestamp', array('default'=>'0000-00-00 00:00:00','comment'=>'最近更新时间'))
+            ->addColumn('create_time', 'timestamp', array('default'=>'CURRENT_TIMESTAMP','comment'=>'创建时间'))
+            ->addColumn('last_visit_time', 'timestamp', array('default'=>'CURRENT_TIMESTAMP','comment'=>'最近更新时间'))
             ->addColumn('session_key', 'string', array('limit' => 100,'default'=>'','comment'=>'会话密钥，用于解密微信的敏感数据'))
             ->addColumn('user_info', 'string', array('limit' => 2048,'default'=>'','comment'=>'用户数据'))
 
