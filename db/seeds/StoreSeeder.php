1ed7caa834b2d36d772a2f99e491fdb21e4a7371
翁晓俊 <wengxj@sdc.icbc.com.cn>
2018-05-17 00:29:48 +0800

    add db seeds

---
+++ db/seeds/StoreSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class StoreSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $store_id = $this->query("select store_id from inv_store where store_name = 'seed店铺'")->fetch()[0];
+        if ($store_id) {
+            $this->execute("delete from inv_stock_sku where stock_id in (select stock_id from inv_store where store_id = {$store_id})");
+            $this->execute("delete from inv_store where store_id = ".$store_id);
+            $this->execute("delete from inv_user_store_relate where store_id = ".$store_id);
+            $this->execute("delete from inv_stock where store_id = ".$store_id);
+        }
+
+        $inv_store = $this->table('inv_store');
+        $inv_store->insert([
+            'store_name' => 'seed店铺',
+            'owner_name' => 'Connor Weng',
+            'store_logo' => '',
+            'description' => '用于演示 API',
+            'region' => '上海',
+            'address' => '市中心',
+            'zipcode' => '200000',
+            'tel' => '57826548',
+            'phone' => '13803974799',
+            'im_ww' => 'wangwang',
+            'im_wx' => 'wxwx',
+            'add_time' => '1526484718',
+            'end_time' => '1626484718'])
+                ->save();
+
+        $store_id = $this->query("select max(store_id) from inv_store")->fetch()[0];
+        $user_id = $this->query("select user_id from inv_user where open_id = 'oRBw65KX7KN23HeMx7mCyJ-u6dbw'")->fetch()[0];
+
+        $this->table('inv_user_store_relate')->insert(['user_id' => $user_id, 'store_id' => $store_id])->save();
+        $this->table('inv_stock')->insert([
+            'store_id' => $store_id,
+            'thumbnail' => '',
+            'product_code' => '6887',
+            'sku_content' => '颜色：红色，尺码：XL',
+            'stock_amount' => 150])->save();
+
+        $stock_id = $this->query('select max(stock_id) from inv_stock')->fetch()[0];
+
+        $this->table('inv_stock_sku')->insert([
+            ['stock_id' => $stock_id, 'color' => '红色', 'size' => 'XL', 'stock_amount' => 50],
+            ['stock_id' => $stock_id, 'color' => '黄色', 'size' => 'L', 'stock_amount' => 50],
+            ['stock_id' => $stock_id, 'color' => '蓝色', 'size' => 'M', 'stock_amount' => 50]])->save();
+    }
+}
---
+++ db/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $this->execute("DELETE FROM wx_session_info where open_id = 'oRBw65KX7KN23HeMx7mCyJ-u6dbw'");
+        $wx_session_info = $this->table('wx_session_info');
+        $wx_session_info->insert([
+            'open_id' => 'oRBw65KX7KN23HeMx7mCyJ-u6dbw',
+            'uuid' => '297b6b794c762bcb5135ed03718ab03f',
+            'skey' => 'e2dc4720465f1ad26990b77ebe6976c0e2c4ce54',
+            'create_time' => '2018-05-07 00:39:06',
+            'last_visit_time' => '2018-05-14 22:13:07',
+            'session_key' => 'zBM9ifrfCGT5/XxdopyUVg==',
+            'user_info' => '{"openId":"oRBw65KX7KN23HeMx7mCyJ-u6dbw","nickName":"Connor.W","gender":1,"language":"zh_CN","city":"Pudong New District","province":"Shanghai","country":"China","avatarUrl":"https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eq5G5iaJNoq4zDSjTYcjV2ZAyydzwEOXBo87Z6nJ0yqA7pvxhkVmJicquHZKAop7tgjDufaics9iawpRA/132","watermark":{"timestamp":1526058180,"appid":"wxf055c7a111fd4d2b"}}'])
+                ->save();
+
+        $this->execute("DELETE FROM inv_user where open_id = 'oRBw65KX7KN23HeMx7mCyJ-u6dbw'");
+        $inv_user = $this->table('inv_user');
+        $inv_user->insert([
+            'open_id' => 'oRBw65KX7KN23HeMx7mCyJ-u6dbw',
+            'wx_nick_name' => 'Connor.W',
+            'wx_avatar_url' => 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eq5G5iaJNoq4zDSjTYcjV2ZAyydzwEOXBo87Z6nJ0yqA7pvxhkVmJicquHZKAop7tgjDufaics9iawpRA/132',
+            'gender' => 1,
+            'phone' => '13808766355',
+            'city' => 'Pudong New District',
+            'province' => 'Shanghai',
+            'country' => 'China'])
+                ->save();
+    }
+}
